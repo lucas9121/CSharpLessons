2324b56915f4f2ff04c1494838271432731ed3c0
Lucas Carlos <lucas2carlos@gmail.com>
2024-04-24 13:14:10 -0400

    creating first c sharp project

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿// See https://aka.ms/new-console-template for more information
+Random dice = new Random();
+
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+
+int total = roll1 + roll2 + roll3;
+
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+{
+    if ((roll1 == roll2) && (roll2 == roll3)) 
+    {
+        Console.WriteLine("You rolled triples! +6 bonus to total!");
+        total += 6;
+    } else{
+        Console.WriteLine("You rolled doubles! +2 bonus to total!");
+        total += 2;
+    }
+}
+
+
+if (total >= 16)
+{
+    Console.WriteLine("You win a new car!");
+}
+else if (total >= 10)
+{
+    Console.WriteLine("You win a new laptop!");
+}
+else if (total == 7)
+{
+    Console.WriteLine("You win a trip for two!");
+}
+else
+{
+    Console.WriteLine("You win a kitten!");
+}
+
+
+Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+// int discountPercentage = 0;
+
+// Your code goes here
+
+if(daysUntilExpiration > 5 && daysUntilExpiration <= 10){
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
+} else if (daysUntilExpiration > 1 && daysUntilExpiration <= 5){
+    Console.Write($"Your subscription expires in ${daysUntilExpiration} days.\nRenew now and save 10%!\n");
+} else if (daysUntilExpiration == 1){
+    Console.Write("Your subscription expires within a day!\nRenew now and save 20%!\n");
+} else {
+    Console.WriteLine("Your subscription has expired.");
+}
+
+
+string[] fraudulentOrderIDs = new string[3];
+
+fraudulentOrderIDs[0] = "A123";
+fraudulentOrderIDs[1] = "B456";
+fraudulentOrderIDs[2] = "C789";
+
+Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
