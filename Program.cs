a5684299316ef1f9be2defa853a1a7db9d807875
Lucas Carlos <lucas2carlos@gmail.com>
2024-04-24 13:20:33 -0400

    creating first c sharp project

--- Program.cs
+++ Program.cs
@@ -66,3 +66,19 @@ fraudulentOrderIDs[2] = "C789";
 Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
 Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
 Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+
+fraudulentOrderIDs[0] = "F000";
+
+Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
+
+string[] names = { "Rowena", "Robin", "Bao" };
+foreach (string name in names)
+{
+    Console.WriteLine(name);
+}
+
+int[] inventory = [200, 450, 700, 175, 250];
+
+foreach(int item in inventory){
+    Console.WriteLine($"item is: {item}");
+}
