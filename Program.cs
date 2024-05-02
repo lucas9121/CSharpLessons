cb0f47102a63273613760401a01ef2a5c6b05ef9
Lucas Carlos <lucas2carlos@gmail.com>
2024-05-02 13:28:25 -0400

    FizzBuzz

--- Program.cs
+++ Program.cs
@@ -360,3 +360,23 @@ switch (product[2])
 }
 
 Console.WriteLine($"Product: {stockSize} {stockColor} {stockType}");
+
+
+//////////////////////////////////////////////// Fizz Buzz //////////////////////////////////////////////////////
+
+
+
+for(int i = 0; i < 10; i++){
+    Console.Write(i);
+    if(i % 3 == 0 && i % 5 == 0){
+        Console.Write(" FizzBuzz\n");
+    } else if(i % 3 == 0){
+        Console.Write(" Fizz\n");
+    } else if(i % 5 == 0){
+        Console.Write(" Buzz\n");
+    } else {
+        Console.Write("\n");
+    }
+}
+
+
--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83244397e58b0cd996c377dbbb3b71ad3ff3749c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+969322428847f85d488e744613e3e87dc6de1504")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
